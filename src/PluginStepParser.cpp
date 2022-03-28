8a5717cbabc1f2630e10acaf8fe90152cf76efe9
Geraldo Ribeiro <geraldo@intmain.io>
2022-03-28 14:42:05 -0300

    Teste para bash

--- src/PluginStepParser.cpp
+++ src/PluginStepParser.cpp
@@ -103,12 +103,11 @@ string PluginStepParser::stepDockerWorkspace(const YAML::Node &step, const strin
 set<EnvironmentVariable> PluginStepParser::parseEnvs(const YAML::Node &step) const {
   auto ret = mMicroCI->DefaultEnvs();
   if (step["envs"] and step["envs"].IsMap()) {
-    for (auto it : step["envs"]) {
+    for (const auto & it : step["envs"]) {
       EnvironmentVariable env;
       env.name = it.first.as<string>();
       env.value = it.second.as<string>();
       ret.insert(env);
-      spdlog::info("Environment variable: {} -> {}", env.name, env.value);
     }
   }
   return ret;
--- test/bash.sh
+++ test/bash.sh
@@ -168,15 +168,17 @@ reformatJson
 # Atualiza as imagens docker utilizadas no passos
 {
   docker pull debian:stable-slim 2>&1
+  docker pull gcc:11 2>&1
 } >> .microCI.log
+# bash 
 
 # ----------------------------------------------------------------------
-# Documentação do projeto
+# Descrição deste passo
 # ----------------------------------------------------------------------
-function step_construir_documentacao_em_formato_html() {
+function step_compilar_versao_estatica_do_microci() {
   SECONDS=0
-  MICROCI_STEP_NAME="Construir documentação em formato HTML"
-  MICROCI_STEP_DESCRIPTION="Documentação do projeto"
+  MICROCI_STEP_NAME="Compilar versão estática do microCI"
+  MICROCI_STEP_DESCRIPTION="Descrição deste passo"
   MICROCI_GIT_ORIGIN=$( git config --get remote.origin.url || echo "SEM GIT ORIGIN" )
   MICROCI_GIT_COMMIT_SHA=$( git rev-parse --short HEAD || echo "SEM GIT COMMIT")
   MICROCI_GIT_COMMIT_MSG=$( git show -s --format=%s )
@@ -186,66 +188,8 @@ function step_construir_documentacao_em_formato_html() {
   title="$(( MICROCI_STEP_NUMBER + 1 )) ${MICROCI_STEP_NAME}.............................................................."
   title=${title:0:60}
   echo -ne "[0;36m${title}[0m: "
-
-  {
-    (
-      set -e
-
-      # shellcheck disable=SC2140
-      docker run \
-        --user $(id -u):$(id -g) \
-        --interactive \
-        --attach stdout \
-        --attach stderr \
-        --rm \
-        --workdir /microci_workspace \
-        --volume "${MICROCI_PWD}":/microci_workspace \
-        --network host \
-        --publish 8000:8000 \
-        intmain/microci_mkdocs_material:latest \
-        mkdocs build 2>&1
-
-    )
-
-    status=$?
-    MICROCI_STEP_DURATION=$SECONDS
-    echo "Status: ${status}"
-    echo "Duration: ${MICROCI_STEP_DURATION}"
-  } >> .microCI.log
-
-  # Notificação no terminal
-  if [ "${MICROCI_STEP_SKIP}" = "yes" ]
-  then
-    echo -e "[0;34mSKIP[0m"
-    setStepStatusSkipJson
-  elif [ "${status}" = "0" ]
-  then
-    echo -e "[0;32mOK[0m"
-    setStepStatusOkJson
-  else
-    echo -e "[0;31mFALHOU[0m"
-    setStepStatusFailJson
-  fi
-
-  ((++MICROCI_STEP_NUMBER))
-}
-
-# ----------------------------------------------------------------------
-# Publica arquivos em um repositório git
-# ----------------------------------------------------------------------
-function step_publicar_html_para_repositorio_git() {
-  SECONDS=0
-  MICROCI_STEP_NAME="Publicar HTML para repositório git"
-  MICROCI_STEP_DESCRIPTION="Publica arquivos em um repositório git"
-  MICROCI_GIT_ORIGIN=$( git config --get remote.origin.url || echo "SEM GIT ORIGIN" )
-  MICROCI_GIT_COMMIT_SHA=$( git rev-parse --short HEAD || echo "SEM GIT COMMIT")
-  MICROCI_GIT_COMMIT_MSG=$( git show -s --format=%s )
-  MICROCI_STEP_STATUS=":ok:"
-  MICROCI_STEP_SKIP="no"
-  MICROCI_STEP_DURATION=$SECONDS
-  title="$(( MICROCI_STEP_NUMBER + 1 )) ${MICROCI_STEP_NAME}.............................................................."
-  title=${title:0:60}
-  echo -ne "[0;36m${title}[0m: "
+  ENV1="valor da variável de ambiente ENV1"
+  ENV2="valor da variável de ambiente ENV2"
 
   {
     (
@@ -254,80 +198,26 @@ function step_publicar_html_para_repositorio_git() {
       echo ""
       echo ""
       echo ""
-      echo "Passo: Publicar HTML para repositório git"
+      echo "Passo: Compilar versão estática do microCI"
       # shellcheck disable=SC2140,SC2046
       docker run \
-        --user $(id -u):$(id -g) \
         --interactive \
         --attach stdout \
         --attach stderr \
         --rm \
         --network bridge \
         --workdir /microci_workspace \
-        --volume "${HOME}/.ssh":"/.microCI_ssh":ro \
+        --env ENV1="valor da variável de ambiente ENV1" \
+        --env ENV2="valor da variável de ambiente ENV2" \
         --volume "${MICROCI_PWD}":"/microci_workspace":rw \
-        "bitnami/git:latest" \
+        "gcc:11" \
         /bin/bash -c "cd /microci_workspace \
-           && cp -Rv /.microCI_ssh /root/.ssh 2>&1 \
-           && chmod 700 /root/.ssh/ 2>&1 \
-           && chmod 644 /root/.ssh/id_rsa.pub 2>&1 \
-           && chmod 600 /root/.ssh/id_rsa 2>&1 \
-           && git clone 'ssh://git@someurl.com/site.git' --depth 1 '/deploy' 2>&1 \
-           && git -C /deploy config user.name  '$(git config --get user.name)' 2>&1 \
-           && git -C /deploy config user.email '$(git config --get user.email)' 2>&1 \
-           && git -C /deploy rm '*' 2>&1 \
-           && cp -rv site/* /deploy/ 2>&1 \
-           && git -C /deploy add . 2>&1 \
-           && git -C /deploy commit -am ':rocket:microCI git_publish' 2>&1 \
-           && git -C /deploy push origin master 2>&1 \
-           && chown $(id -u):$(id -g) -Rv site 2>&1
-  "
-    )
-
-    status=$?
-    MICROCI_STEP_DURATION=$SECONDS
-    echo "Status: ${status}"
-    echo "Duration: ${MICROCI_STEP_DURATION}"
-  } >> .microCI.log
-
-  # Notificação no terminal
-  if [ "${MICROCI_STEP_SKIP}" = "yes" ]
-  then
-    echo -e "[0;34mSKIP[0m"
-    setStepStatusSkipJson
-  elif [ "${status}" = "0" ]
-  then
-    echo -e "[0;32mOK[0m"
-    setStepStatusOkJson
-  else
-    echo -e "[0;31mFALHOU[0m"
-    setStepStatusFailJson
-  fi
-
-  ((++MICROCI_STEP_NUMBER))
-}
-
-# ----------------------------------------------------------------------
-# Executa servidor local para preview da documentação
-# ----------------------------------------------------------------------
-function step_servidor_local_na_porta_8000__ctrl_c_para_finalizar_() {
-  SECONDS=0
-  MICROCI_STEP_NAME="Servidor local na porta 8000 (Ctrl+C para finalizar)"
-  MICROCI_STEP_DESCRIPTION="Executa servidor local para preview da documentação"
-  MICROCI_GIT_ORIGIN=$( git config --get remote.origin.url || echo "SEM GIT ORIGIN" )
-  MICROCI_GIT_COMMIT_SHA=$( git rev-parse --short HEAD || echo "SEM GIT COMMIT")
-  MICROCI_GIT_COMMIT_MSG=$( git show -s --format=%s )
-  MICROCI_STEP_STATUS=":ok:"
-  MICROCI_STEP_SKIP="yes"
-  MICROCI_STEP_DURATION=$SECONDS
-  title="$(( MICROCI_STEP_NUMBER + 1 )) ${MICROCI_STEP_NAME}.............................................................."
-  title=${title:0:60}
-  echo -ne "[0;36m${title}[0m: "
-
-  {
-    (
-      set -e
-      echo 'Este passo não faz nada'
+           && apt update 2>&1 \
+           && apt upgrade -y 2>&1 \
+           && apt install -y xxd libspdlog-dev libyaml-cpp-dev 2>&1 \
+           && make -C src clean all 2>&1 \
+           && make -C test 2>&1 \
+           && chown $(id -u).$(id -g) -Rv src/microCI 2>&1"
 
     )
 
@@ -359,9 +249,7 @@ function step_servidor_local_na_porta_8000__ctrl_c_para_finalizar_() {
 function main() {
   date >> .microCI.log
 
-  step_construir_documentacao_em_formato_html
-  step_publicar_html_para_repositorio_git
-  step_servidor_local_na_porta_8000__ctrl_c_para_finalizar_
+  step_compilar_versao_estatica_do_microci
 
   date >> .microCI.log
 }
