2a1771d35ca8e5f98538c5bfeb0ab946381d1c03
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2024-02-22 16:10:25 +0400

    clang image 0.6

--- src/ClangTidyPluginStepParser.cpp
+++ src/ClangTidyPluginStepParser.cpp
@@ -69,7 +69,7 @@ void ClangTidyPluginStepParser::Parse(const YAML::Node &step) {
   }
 
   data["STEP_NAME"] = stepName(step);
-  data["DOCKER_IMAGE"] = stepDockerImage(step, "intmain/microci_clang:latest");
+  data["DOCKER_IMAGE"] = stepDockerImage(step, "intmain/microci_clang:0.6");
   data["FUNCTION_NAME"] = sanitizeName(stepName(step));
   data["STEP_DESCRIPTION"] = stepDescription(step, "Verifica código C++");
 
