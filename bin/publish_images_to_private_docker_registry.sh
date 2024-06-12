afdab650ab9c2e760150dd78365489a6846d266f
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2024-06-12 08:40:33 +0400

    Avoid send image already on the registry

--- bin/publish_images_to_private_docker_registry.sh
+++ bin/publish_images_to_private_docker_registry.sh
@@ -5,7 +5,7 @@
 
 DOCKER_REGISTRY_IP=192.168.1.11
 
-for image in $(docker image ls | grep microci | grep -v none | sed "s/\s\+/:/; s/\s.*//")
+for image in $(docker image ls | grep microci | grep -v "$DOCKER_REGISTRY_IP" | grep -v none | sed "s/\s\+/:/; s/\s.*//")
 do
 	docker tag ${image} ${DOCKER_REGISTRY_IP}:5000/${image}
 	docker push ${DOCKER_REGISTRY_IP}:5000/${image}
