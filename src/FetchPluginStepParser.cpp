8e70d7d1b8a1efef3cf4e919ea4824fc99a55db1
Geraldo Ribeiro <geraldolsribeiro@gmail.com>
2025-02-24 11:32:14 +0400

    Fix alignment

--- src/FetchPluginStepParser.cpp
+++ src/FetchPluginStepParser.cpp
@@ -215,8 +215,8 @@ void FetchPluginStepParser::Parse(const YAML::Node &step) {
     data["TARGET"] = defaultTarget;
     mMicroCI->Script() << inja::render(
         R"( \
-          ; echo "Default target chown" \
-          ; chown $(id -u):$(id -g) -Rv {{ TARGET }})",
+           ; echo "Default target chown" \
+           ; chown $(id -u):$(id -g) -Rv {{ TARGET }})",
         data);
     mMicroCI->Script() << "\"\n";
     endFunction(data);
